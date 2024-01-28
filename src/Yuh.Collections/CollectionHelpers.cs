0856ab9cfb09fc8e55a8ece71181bfaeafac6d04
YuHima <yuhima03@gmail.com>
2024-01-28 17:04:11 +0900

    [Add] Added CollectionHelpers class.

---
+++ src/Yuh.Collections/CollectionHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yuh.Collections
+{
+    internal static class CollectionHelpers
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void SetDefaultValueIfReferenceOrContainsReferences<T>(ref T value)
+        {
+            if (RuntimeHelpers.IsReferenceOrContainsReferences<T>())
+            {
+                value = default!;
+            }
+        }
+    }
+}
