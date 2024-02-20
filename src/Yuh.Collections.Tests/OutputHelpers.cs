17b0a879834286e62874983a4526f40e0768c7da
YuHima <yuhima03@gmail.com>
2024-02-21 00:09:45 +0900

    test: add the OutputHelpers class

---
+++ src/Yuh.Collections.Tests/OutputHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Yuh.Collections.Tests
+{
+    internal static class OutputHelpers
+    {
+        internal static void OutputElements<T>(IEnumerable<T> buffer, ITestOutputHelper @out)
+        {
+            StringBuilder sb = new();
+
+            foreach (var v in buffer)
+            {
+                sb.Append(v?.ToString()).Append('\x20');
+            }
+
+            @out.WriteLine(sb.ToString());
+        }
+    }
+}
