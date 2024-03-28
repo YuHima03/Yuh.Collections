0ea7a2637a8e41bbb0c751684fdf6d0f0311547b
YuHima <yuhima03@gmail.com>
2024-03-29 02:14:24 +0900

    feat: add a new class
    A new class: Views.DequeSlimView{T}.

---
+++ src/Yuh.Collections.Views/DequeSlimView.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Yuh.Collections.Views
+{
+    public class DequeSlimView<T>
+    {
+        // Do NOT change the order of the following fields.
+        public T[] Items = [];
+        public int Capacity;
+        public int Count;
+        public int Head;
+        public int Version;
+
+        public DequeSlim<T> ToDequeSlim()
+        {
+            return Unsafe.As<DequeSlim<T>>(this);
+        }
+    }
+}
