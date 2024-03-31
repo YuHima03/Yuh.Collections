6828b33d09b8c3faf3d97fd524afe4ca9e4abbd6
YuHima <yuhima03@gmail.com>
2024-03-31 17:16:32 +0900

    feat: add a new class: DequeView{T}

---
+++ src/Yuh.Collections.Views/DequeView.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Yuh.Collections.Views
+{
+    public class DequeView<T>
+    {
+        public T[] Items = [];
+        public int Count;
+        public int Head;
+        public int Version;
+
+        public Deque<T> Encapsulate()
+        {
+            return Unsafe.As<Deque<T>>(this);
+        }
+    }
+}
