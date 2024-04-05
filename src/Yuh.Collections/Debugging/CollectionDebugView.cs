18559b34f9667da1208cacaa05b67bbc361ddcdf
YuHima <yuhima03@gmail.com>
2024-04-05 19:06:29 +0900

    feat: add a new class, CollectionDebugView{T} for debugging

---
+++ src/Yuh.Collections/Debugging/CollectionDebugView.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace Yuh.Collections.Debugging
+{
+    [DebuggerNonUserCode]
+    internal class CollectionDebugView<T>
+    {
+        private readonly ICollection<T> _collection;
+
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public T[] Items => _collection.ToArray();
+
+        public CollectionDebugView(ICollection<T> collection)
+        {
+            ArgumentNullException.ThrowIfNull(collection);
+            _collection = collection;
+        }
+    }
+}
