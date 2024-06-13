cf8d354988549d63292ef389a69abf319af57458
YuHima03 <yuhima03@gmail.com>
2024-06-13 10:16:26 +0900

    feat: new structure, CollectionBuilder<T>

---
+++ src/Yuh.Collections/CollectionBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Yuh.Collections
+{
+    /// <summary>
+    /// Repreents a temporary collection that is used to build new collections.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    public ref struct CollectionBuilder<T>
+    {
+    }
+}
