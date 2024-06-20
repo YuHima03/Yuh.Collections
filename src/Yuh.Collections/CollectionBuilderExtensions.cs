084de3d9bf31c98f1f8d41b7f0d173450d091334
YuHima03 <yuhima03@gmail.com>
2024-06-21 07:56:18 +0900

    feat: a new static class CollectionBuilderExtensions

---
+++ src/Yuh.Collections/CollectionBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Buffers;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Yuh.Collections
+{
+    public static class CollectionBuilderExtensions
+    {
+    }
+}
