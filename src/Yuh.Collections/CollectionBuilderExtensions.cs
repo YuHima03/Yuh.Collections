c53298320677101deedbc049c01f71fd9df903f2
YuHima03 <yuhima03@gmail.com>
2024-06-21 07:56:55 +0900

    feat: new public methods ToString and ToStringBuilder

--- src/Yuh.Collections/CollectionBuilderExtensions.cs
+++ src/Yuh.Collections/CollectionBuilderExtensions.cs
@@ -6,5 +6,47 @@ namespace Yuh.Collections
 {
     public static class CollectionBuilderExtensions
     {
+        public static string ToString(in this CollectionBuilder<char> builder)
+        {
+            int length = builder.Count;
+            if (length <= 1024)
+            {
+                Span<char> chars = stackalloc char[length];
+                builder.CopyTo(chars);
+                return new(chars);
+            }
+            else
+            {
+                var charsArray = ArrayPool<char>.Shared.Rent(length);
+                var chars = MemoryMarshal.CreateSpan(ref MemoryMarshal.GetArrayDataReference(charsArray), length);
+                builder.CopyTo(chars);
+                string s = new(chars);
+
+                ArrayPool<char>.Shared.Return(charsArray);
+                return s;
+            }
+        }
+
+        public static StringBuilder ToStringBuilder(in this CollectionBuilder<char> builder)
+        {
+            int length = builder.Count;
+            StringBuilder sb = new(length);
+            if (length <= 1024)
+            {
+                Span<char> chars = stackalloc char[length];
+                builder.CopyTo(chars);
+                return sb.Append(chars);
+            }
+            else
+            {
+                var charsArray = ArrayPool<char>.Shared.Rent(length);
+                var chars = MemoryMarshal.CreateSpan(ref MemoryMarshal.GetArrayDataReference(charsArray), length);
+                builder.CopyTo(chars);
+                sb.Append(chars);
+
+                ArrayPool<char>.Shared.Return(charsArray);
+                return sb;
+            }
+        }
     }
 }
