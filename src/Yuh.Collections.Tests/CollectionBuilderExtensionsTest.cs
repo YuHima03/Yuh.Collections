e32226e224ac461d035f26dea1b32c7e7dcc2eb9
YuHima03 <yuhima03@gmail.com>
2024-06-22 17:55:57 +0900

    test: add test for the CollectionBuilderExtensions class

---
+++ src/Yuh.Collections.Tests/CollectionBuilderExtensionsTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Yuh.Collections.Tests
+{
+    public class CollectionBuilderExtensionsTest
+    {
+        [Fact]
+        public void ToStringTest()
+        {
+            CollectionBuilder<char> builder = new();
+            builder.AddRange("foo\n".AsSpan());
+            builder.AddRange("bar\n".AsSpan());
+            builder.AddRange("qux\n".AsSpan());
+            builder.AddRange("The quick brown fox jumps over the lazy dog.".AsSpan());
+
+            Assert.True(builder.ToStringBuilder().ToString() == "foo\nbar\nqux\nThe quick brown fox jumps over the lazy dog.");
+        }
+    }
+}
