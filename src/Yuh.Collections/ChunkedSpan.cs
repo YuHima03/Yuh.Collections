dfb79eb59cbc9f2342f30a1c4479e857bdf7e8f2
YuHima03 <yuhima03@gmail.com>
2024-10-21 12:24:48 +0900

    feat: ref struct ChunkedSpan{T}

---
+++ src/Yuh.Collections/ChunkedSpan.cs
@@ -0,0 +1,13 @@
+﻿namespace Yuh.Collections
+{
+    internal ref struct ChunkedSpan<T, TSpanProvider>
+#if NET9_0_OR_GREATER
+        where TSpanProvider : struct, IReadOnlyList<Span<T>>
+#else
+        where TSpanProvider : struct, IReadOnlySpanList<T>
+#endif
+    {
+        private TSpanProvider _spans;
+        private Span<int> _lengthSum;
+    }
+}
