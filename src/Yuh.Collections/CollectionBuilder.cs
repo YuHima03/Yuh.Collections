1a7294edcff8936a119997dca001d8864af90452
Yusuke.T <yuhima03@gmail.com>
2024-10-23 20:38:18 +0900

    chore: remove unsafe mark from CollectionBuilder{T}

--- src/Yuh.Collections/CollectionBuilder.cs
+++ src/Yuh.Collections/CollectionBuilder.cs
@@ -25,7 +25,7 @@ namespace Yuh.Collections
     /// Represents a temporary collection that is used to build new collections.
     /// </summary>
     /// <typeparam name="T">The type of elements in the collection.</typeparam>
-    public unsafe ref struct CollectionBuilder<T>
+    public ref struct CollectionBuilder<T>
 #if NET9_0_OR_GREATER
         : IEnumerable<T>, IReadOnlyCollection<T>
 #endif
