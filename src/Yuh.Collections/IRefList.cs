8f4c718c14053292bdb5ff01c0d1793759b3377d
Yusuke.T <yuhima03@gmail.com>
2024-11-05 17:32:17 +0900

    feat: IRefList interface

---
+++ src/Yuh.Collections/IRefList.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Yuh.Collections
+{
+    public interface IRefList<T>
+    {
+        public int Count { get; }
+
+        public Span<T> this[int index] { get; }
+    }
+}
