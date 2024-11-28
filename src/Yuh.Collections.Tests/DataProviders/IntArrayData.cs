1c0661b16485022628dfcbace63a00aefadf8007
Yusuke.T <yuhima03@gmail.com>
2024-11-29 01:20:26 +0900

    feat: data provider class for testing

---
+++ src/Yuh.Collections.Tests/DataProviders/IntArrayData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using Yuh.Collections.Tests.Helpers;
+
+namespace Yuh.Collections.Tests.DataProviders
+{
+    public class IntArrayData : IEnumerable<object[]>
+    {
+        public readonly static int[][] DataSource = [.. SegmentedIntArrayData.DataSource.Select(arrays => arrays.Flatten().ToArray())];
+
+        public IEnumerator<object[]> GetEnumerator() => DataSource.Select<int[], object[]>(x => [x]).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
