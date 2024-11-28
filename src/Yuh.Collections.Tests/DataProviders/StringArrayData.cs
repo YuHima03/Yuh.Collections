4371a388dfcd49e52d2498797f6c009985897de1
Yusuke.T <yuhima03@gmail.com>
2024-11-28 18:12:44 +0900

    test: test data provider classes

---
+++ src/Yuh.Collections.Tests/DataProviders/StringArrayData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+namespace Yuh.Collections.Tests.DataProviders
+{
+    public sealed class StringArrayData() : IEnumerable<object[]>
+    {
+        public readonly static string[][] DataSource = [
+            [],
+            ["foo", "bar", "baz", "qax"],
+            [.. Enumerable.Repeat("The quick brown fox jumps over the lazy dog\r\n", 1024)]
+        ];
+
+        public IEnumerator<object[]> GetEnumerator() => DataSource.Select<string[], object[]>(x => [x]).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
