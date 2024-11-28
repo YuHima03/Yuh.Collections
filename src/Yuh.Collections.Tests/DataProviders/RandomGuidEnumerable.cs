54da4d00328ea53880bf8deea5df6c7eb7d86a8e
Yusuke.T <yuhima03@gmail.com>
2024-11-28 19:45:31 +0900

    feat: test data provider

---
+++ src/Yuh.Collections.Tests/DataProviders/RandomGuidEnumerable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace Yuh.Collections.Tests.DataProviders
+{
+    public class RandomGuidEnumerable : IEnumerable<Guid>
+    {
+        private readonly int _length;
+        private readonly Random _random;
+
+        public int Count => _length;
+
+        public RandomGuidEnumerable(int length)
+        {
+            _length = length;
+            _random = new();
+        }
+
+        public RandomGuidEnumerable(int length, int seed)
+        {
+            _length = length;
+            _random = new(seed);
+        }
+
+        public IEnumerator<Guid> GetEnumerator()
+        {
+            byte[] buffer = new byte[16];
+            for (int i = 0; i < _length; i++)
+            {
+                _random.NextBytes(buffer);
+                yield return new Guid(buffer);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
