8c14e80b13a3f84da34ada05663594c9eaeef4c3
Yusuke.T <yuhima03@gmail.com>
2024-11-28 18:13:23 +0900

    test: tests for CollectionBuilderExtensions class

--- src/Yuh.Collections.Tests/CollectionBuilderExtensionsTest.cs
+++ src/Yuh.Collections.Tests/CollectionBuilderExtensionsTest.cs
@@ -1,17 +1,67 @@
-﻿namespace Yuh.Collections.Tests
+﻿using System.Runtime.CompilerServices;
+using Yuh.Collections.Tests.DataProviders;
+using Yuh.Collections.Tests.Helpers;
+
+namespace Yuh.Collections.Tests
 {
     public class CollectionBuilderExtensionsTest
     {
-        [Fact]
-        public void ToBasicStringTest()
+        [Theory]
+        [ClassData(typeof(SegmentedIntArrayData))]
+        public void ToDoubleEndedListTest(int[][] items)
         {
-            CollectionBuilder<char> builder = new();
-            builder.AppendRange("foo\n".AsSpan());
-            builder.AppendRange("bar\n".AsSpan());
-            builder.AppendRange("qux\n".AsSpan());
-            builder.AppendRange("The quick brown fox jumps over the lazy dog.".AsSpan());
+            using CollectionBuilder<int> builder = new();
 
-            Assert.Equal("foo\nbar\nqux\nThe quick brown fox jumps over the lazy dog.", builder.ToBasicString());
+            foreach (var array in items)
+            {
+                builder.AppendRange(array.AsSpan());
+            }
+
+            Assert.Equal(items.Flatten(), builder.ToDoubleEndedList());
+        }
+
+        [Theory]
+        [ClassData(typeof(SegmentedIntArrayData))]
+        public void ToListTest(int[][] items)
+        {
+            using CollectionBuilder<int> builder = new();
+
+            foreach (var array in items)
+            {
+                builder.AppendRange(array.AsSpan());
+            }
+
+            Assert.Equal(items.Flatten().ToList(), builder.ToList());
+        }
+
+        [Theory]
+        [ClassData(typeof(StringArrayData))]
+        public void ToBasicStringTest(string[] items)
+        {
+            using CollectionBuilder<char> builder = new();
+            DefaultInterpolatedStringHandler handler = new(0, items.Length);
+
+            foreach (var str in items)
+            {
+                builder.AppendRange(str.AsSpan());
+                handler.AppendFormatted(str);
+            }
+
+            Assert.Equal(handler.ToStringAndClear(), builder.ToBasicString());
+        }
+
+        [Theory]
+        [ClassData(typeof(StringArrayData))]
+        public void ToStringBuilderTest(string[] items)
+        {
+            using CollectionBuilder<char> builder = new();
+
+            foreach (var str in items)
+            {
+                builder.AppendRange(str.AsSpan());
+            }
+
+            Assert.Equal(builder.ToBasicString(), builder.ToStringBuilder().ToString());
         }
     }
 }
