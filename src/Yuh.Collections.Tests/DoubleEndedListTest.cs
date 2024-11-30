04a9912cbdc0f85dd0f3f3719b3f6716ab782039
Yusuke.T <yuhima03@gmail.com>
2024-11-30 13:15:11 +0900

    test: add tests for AsReadOnlySpan and Clear methods

--- src/Yuh.Collections.Tests/DoubleEndedListTest.cs
+++ src/Yuh.Collections.Tests/DoubleEndedListTest.cs
@@ -6,6 +6,30 @@ namespace Yuh.Collections.Tests
     {
         [Theory]
         [ClassData(typeof(IntArrayData))]
+        public void AsReadOnlySpanTest(int[] data)
+        {
+            DoubleEndedList<int> list = new(data.AsSpan());
+            Assert.Equal(data.AsSpan(), list.AsReadOnlySpan());
+        }
+
+        [Theory]
+        [ClassData(typeof(SegmentedIntArrayData))]
+        public void ClearTest(int[][] data)
+        {
+            DoubleEndedList<int> list = new();
+
+            foreach (var array in data)
+            {
+                list.PushBackRange(array.AsSpan());
+                Assert.Equal(array.AsSpan(), list.AsReadOnlySpan());
+
+                list.Clear();
+                Assert.Equal([], list.AsReadOnlySpan());
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(IntArrayData))]
         public void InitializeTest(int[] items)
         {
             DoubleEndedList<int> list = new(items as IEnumerable<int>);
