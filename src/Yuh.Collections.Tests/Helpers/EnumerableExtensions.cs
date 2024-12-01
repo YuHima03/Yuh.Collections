1d479f9724cb3d094cc199b5b3309b275dd2d298
Yusuke.T <yuhima03@gmail.com>
2024-12-02 02:25:31 +0900

    feat: new extension method for IEnumerable{T}

--- src/Yuh.Collections.Tests/Helpers/EnumerableExtensions.cs
+++ src/Yuh.Collections.Tests/Helpers/EnumerableExtensions.cs
@@ -4,5 +4,15 @@
     {
         public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> enumerable)
             => enumerable.Aggregate(Enumerable.Empty<T>(), (accum, item) => accum.Concat(item));
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> enumerable, int? seed = null)
+        {
+            Random rand = seed.HasValue ? new(seed.Value) : new();
+            PriorityQueue<T, int> pq = new(enumerable.Select(x => (x, rand.Next())));
+            while (pq.Count != 0)
+            {
+                yield return pq.Dequeue();
+            }
+        }
     }
 }
